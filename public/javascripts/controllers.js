677db3d291617120d856d9e83578f2ec88f7231a
mikelimassol <mikelimassol@hotmail.com>
2015-09-08 23:36:56 +0100

    I have retrieved results from foursquare and displayed on home page

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -4,6 +4,24 @@ angular.module('FourSquareApp')
 .controller('MainCtrl', [ 'PlacesService', function(PlacesService) {
 	
 	var self = this;
+
 	self.helloWorld = "hello world";
+	
+	self.getConnectedUsers = function() {
+		PlacesService
+				.browsePlaces()
+				.then(
+						function(result) {
+							if (result.data == 0) {
+								self.errorMessage = "No Places";
+							}
+							self.places = result.data;
+							return result;
+						}, function(error) {
+
+						})
+	}
+	
+	self.getConnectedUsers();
 
 } ]);
